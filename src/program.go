4ab430088bcab7562f3e1b8b124f4e877cde91a9
Kingfish <https.mohammad@gmail.com>
2023-12-07 21:10:42 +0330

    Adds setting dns

--- src/program.go
+++ src/program.go
@@ -2,17 +2,25 @@ package main
 
 import (
 	"fmt"
+	"net"
 	"os/exec"
+	"strings"
 )
 
 func main() {
-	// interfaceName := "Local Area Connection"
 
-	// dns1 := "8.8.8.8"
+	var activeInterface = getActiveNetworkInterface()
+	if activeInterface == "" {
+		fmt.Print("Failed getting active network interface")
+		return
+	}
+
+	dns1 := "8.8.8.8"
 	dns2 := "8.8.4.4"
 
-	// commandText := fmt.Sprintf(`Set-DNSClientServerAddress -InterfaceAlias "%s" -ServerAddresses ("%s","%s")`, interfaceName, dns1, dns2)
-	commandText := fmt.Sprintf(`ping "%s"`, dns2)
+	commandText := fmt.Sprintf(`Set-DNSClientServerAddress -InterfaceAlias "%s" -ServerAddresses ("%s","%s")`,
+		activeInterface, dns1, dns2)
+	//commandText := fmt.Sprintf(`ping "%s"`, dns2)
 
 	cmd := exec.Command("powershell", "-Command", commandText)
 
@@ -25,4 +33,20 @@ func main() {
 
 	fmt.Println(output)
 	fmt.Println("DNS settings updated successfully.")
+}
+
+func getActiveNetworkInterface() string {
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		fmt.Println("Error getting network interfaces:", err)
+		return ""
+	}
+
+	for _, intf := range interfaces {
+		if intf.Flags&net.FlagUp != 0 && !strings.Contains(intf.Flags.String(), "loopback") {
+			return intf.Name
+		}
+	}
+
+	return ""
 }
