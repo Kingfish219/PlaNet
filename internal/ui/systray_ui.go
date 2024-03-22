de00413801ef4b5dd0c48a1e83a32ec1497b2d80
Kingfish <https.mohammad@gmail.com>
2024-03-23 01:49:19 +0330

    Adds dns sub menu

--- internal/ui/systray_ui.go
+++ internal/ui/systray_ui.go
@@ -6,13 +6,15 @@ import (
 
 	"github.com/Kingfish219/PlaNet/internal/domain"
 	"github.com/Kingfish219/PlaNet/internal/interfaces"
+	"github.com/Kingfish219/PlaNet/internal/presets"
 	"github.com/Kingfish219/PlaNet/network"
 	"github.com/getlantern/systray"
 )
 
 type SystrayUI struct {
-	dnsRepository     interfaces.DnsRepository
-	dnsConfigurations []domain.Dns
+	dnsRepository            interfaces.DnsRepository
+	dnsConfigurations        []domain.Dns
+	selectedDnsConfiguration domain.Dns
 }
 
 func NewSystrayUI(dnsRepository interfaces.DnsRepository) *SystrayUI {
@@ -70,16 +72,35 @@ func (systrayUI *SystrayUI) addDnsConfigurations() error {
 	if err != nil {
 		return err
 	}
+
+	if len(dnsConfigurations) == 0 {
+		presetDnsList := presets.GetDnsConfigurations()
+		for _, pre := range presetDnsList {
+			systrayUI.dnsRepository.ModifyDnsConfigurations(pre)
+		}
+
+		dnsConfigurations, err = systrayUI.dnsRepository.GetDnsConfigurations()
+		if err != nil {
+			return err
+		}
+	}
+
 	systrayUI.dnsConfigurations = dnsConfigurations
 
-	// var shecan = domain.Dns{
-	// 	Name:         "Shecan",
-	// 	PrimaryDns:   "185.51.200.2",
-	// 	SecendaryDns: "178.22.122.100",
-	// }
+	var dnsConfigSubMenu *systray.MenuItem
 
-	menuSet := systray.AddMenuItem("Set", "Set DNS")
-	menuReset := systray.AddMenuItem("Reset", "Reset DNS")
+	dnsConfigMenu := systray.AddMenuItem(fmt.Sprintf("DNS config: %v", systrayUI.dnsConfigurations[0].Name), "Selected DNS Configuration")
+	for _, dns := range systrayUI.dnsConfigurations {
+		dnsConfigSubMenu = dnsConfigMenu.AddSubMenuItem(dns.Name, dns.Name)
+	}
+
+	menuSet := systray.AddMenuItem("Set DNS", "Set DNS")
+	menuReset := systray.AddMenuItem("Reset DNS", "Reset DNS")
+
+	go func() {
+		<-dnsConfigSubMenu.ClickedCh
+		fmt.Println(dnsConfigSubMenu.String())
+	}()
 
 	go func() {
 		<-menuSet.ClickedCh
