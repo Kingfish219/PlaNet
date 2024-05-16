8dd668c795fbd68cc309521ff08b723eda732855
Maryam Yazdani <yazdanimaryam7@gmail.com>
2024-05-16 15:30:05 +0330

    Adds ui models

--- internal/ui/console/console.go
+++ internal/ui/console/console.go
@@ -1,8 +1,17 @@
 package console
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"atomicgo.dev/keyboard"
+	"atomicgo.dev/keyboard/keys"
+	"github.com/Kingfish219/PlaNet/internal/ui"
+)
 
 type ConsoleUI struct {
+	Name       string
+	ActivePage ui.Page
 }
 
 func New() *ConsoleUI {
@@ -10,7 +19,41 @@ func New() *ConsoleUI {
 }
 
 func (console *ConsoleUI) Initialize() error {
-	_, err := fmt.Println("Welcome")
+	console.drawLogo()
+	FeedMainPage(console)
+
+	fmt.Println()
+	_, err := fmt.Println("What do you want to do?")
+	console.addActions()
+	console.captureKeyboard()
 
 	return err
+}
+
+func (console *ConsoleUI) captureKeyboard() {
+	keyboard.Listen(func(key keys.Key) (stop bool, err error) {
+		switch key.Code {
+		case keys.CtrlC:
+			os.Exit(0)
+			return true, nil
+		case keys.RuneKey:
+			fmt.Printf("\rYou pressed the rune key: %s\n", key)
+		default:
+			fmt.Printf("\rYou pressed: %s\n", key)
+		}
+
+		return false, nil
+	})
+}
+
+func (console *ConsoleUI) drawLogo() {
+	fmt.Println("=======================================")
+	fmt.Println("           Welcome to PlaNet           ")
+	fmt.Println("=======================================")
+}
+
+func (console *ConsoleUI) addActions() {
+	for index, action := range console.ActivePage.Actions {
+		fmt.Println(fmt.Sprint(index+1)+".", action.Title)
+	}
 }
