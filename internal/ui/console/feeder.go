b0cac4ee28a27b45c1a9ef5f6470c206e5d8d24e
Kingfish <https.mohammad@gmail.com>
2024-05-16 18:21:12 +0330

    Adds the building methods

--- internal/ui/console/feeder.go
+++ internal/ui/console/feeder.go
@@ -2,41 +2,53 @@ package console
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/Kingfish219/PlaNet/internal/ui"
 )
 
-func FeedMainPage(console *ConsoleUI) {
+func FeedUI(console *ConsoleUI) {
 	console.ActivePage = MainPage()
 }
 
 func MainPage() ui.Page {
 	return ui.Page{
-		Key: "main",
+		Key:   "main",
+		Title: "What do you want to do?",
 		Items: []ui.Item{
 			{
-				Key:   "main_dns",
-				Title: "DNS Management",
+				Key:      "main_dns",
+				Title:    "1. DNS Management",
+				ShortKey: "1",
 				Page: ui.Page{
-					Key: "main_dns_dns",
+					Key:   "main_dns_dns",
+					Title: "Dns Management",
+					Items: []ui.Item{
+						{
+							Key:   "main_dns_dns_config",
+							Title: "Select",
+						},
+					},
 				},
 			},
 			{
-				Title: "Network Interface Management",
-				Exec: func() {
-					fmt.Println("Test")
-				},
+				Key:      "main_ni",
+				Title:    "2. Network Interface Management",
+				ShortKey: "2",
 			},
 			{
-				Title: "Tools",
-				Exec: func() {
-					fmt.Println("Test")
-				},
+				Key:      "main_tools",
+				Title:    "3. Tools",
+				ShortKey: "3",
 			},
 			{
-				Title: "Exit",
+				Key:      "main_exit",
+				Title:    "0. Exit",
+				ShortKey: "0",
 				Exec: func() {
-					fmt.Println("Test")
+					fmt.Println()
+					fmt.Println("Goodbye...")
+					os.Exit(0)
 				},
 			},
 		},
