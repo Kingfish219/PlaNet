181a92a81dec9d37844f6011c0ce69e9bed86a3a
Kingfish <https.mohammad@gmail.com>
2024-05-16 17:09:48 +0330

    Adds key to models

--- internal/ui/console/feeder.go
+++ internal/ui/console/feeder.go
@@ -12,9 +12,29 @@ func FeedMainPage(console *ConsoleUI) {
 
 func MainPage() ui.Page {
 	return ui.Page{
+		Key: "main",
 		Items: []ui.Item{
 			{
+				Key:   "main_dns",
 				Title: "DNS Management",
+				Page: ui.Page{
+					Key: "main_dns_dns",
+				},
+			},
+			{
+				Title: "Network Interface Management",
+				Exec: func() {
+					fmt.Println("Test")
+				},
+			},
+			{
+				Title: "Tools",
+				Exec: func() {
+					fmt.Println("Test")
+				},
+			},
+			{
+				Title: "Exit",
 				Exec: func() {
 					fmt.Println("Test")
 				},
