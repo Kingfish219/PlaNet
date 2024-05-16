1f4b6e582050efcc055986996998c4a03bae7056
Maryam Yazdani <yazdanimaryam7@gmail.com>
2024-05-16 15:19:02 +0330

    cahnge systray

--- internal/ui/menu/systray/systray.go
+++ internal/ui/menu/systray/systray.go
@@ -1,12 +1,8 @@
 package systray
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
-	"os/exec"
-	"path/filepath"
 
 	"github.com/Kingfish219/PlaNet/internal/interfaces"
 	"github.com/Kingfish219/PlaNet/internal/presets"
@@ -94,7 +90,9 @@ func (systrayUI *SystrayUI) addDnsConfigurations() error {
 
 	dnsMenus := map[string]*systray.MenuItem{}
 
-	dnsConfigMenu := systray.AddMenuItem(fmt.Sprintf("DNS config: %v", systrayUI.dnsConfigurations[0].Name), "Selected DNS Configuration")
+	dnsMenu := systray.AddMenuItem("DNS", "DNS")
+	dnsConfigMenu := dnsMenu.AddSubMenuItem(fmt.Sprintf("Config: %v", systrayUI.dnsConfigurations[0].Name), "Selected DNS Configuration")
+
 	for _, dnsConfig := range systrayUI.dnsConfigurations {
 
 		dnsConfigSubMenu := dnsConfigMenu.AddSubMenuItem(dnsConfig.Name, dnsConfig.Name)
@@ -121,10 +119,76 @@ func (systrayUI *SystrayUI) addDnsConfigurations() error {
 		}(localDns)
 	}
 
-	menuSet := systray.AddMenuItem("Set DNS", "Set DNS")
-	menuReset := systray.AddMenuItem("Reset DNS", "Reset DNS")
-	menuAdd := systray.AddMenuItem("Add DNS", "Add DNS")
-	menuDelete := systray.AddMenuItem("Delete Current DNS", "Delete Current DNS")
+	menuAdd := dnsConfigMenu.AddSubMenuItem("Add New Config", "Add New Config")
+
+	menuSet := dnsMenu.AddSubMenuItem("Set", "Set DNS")
+	menuReset := dnsMenu.AddSubMenuItem("Reset", "Reset DNS")
+	menuDelete := dnsMenu.AddSubMenuItem("Delete This Config", "Delete This Config")
+
+	networkInterfaceMenu := systray.AddMenuItem("Network Interface", "Network Interface")
+	toolsMenu := systray.AddMenuItem("Tools", "Tools")
+	consoleMenu := systray.AddMenuItem("Console", "Console")
+
+	go func() {
+		for {
+			<-menuAdd.ClickedCh
+			fmt.Println("Add new dns")
+
+			newDns := dns.Dns{Name: "MyTest", PrimaryDns: "185.51.200.2", SecendaryDns: "178.22.122.100"}
+			// newDns := openCMDAndGetDNSData()
+
+			if dnsMenus[newDns.Name] != nil {
+				fmt.Println(newDns.Name + " existed")
+				return
+			}
+
+			err := systrayUI.dnsRepository.ModifyDnsConfigurations(newDns)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			systrayUI.selectedDnsConfiguration = newDns
+
+			dnsService := dns.DnsService{}
+			_, err = dnsService.ChangeDns(dns.SetDns, systrayUI.connectedDnsConfiguration)
+			if err != nil {
+				fmt.Println(err)
+
+				return
+			}
+
+			fmt.Println(newDns.Name + " connected successfully.")
+
+			systrayUI.setIcon(true)
+			systrayUI.setToolTip("connected to : " + newDns.Name)
+
+			dnsConfigSubMenu := dnsConfigMenu.AddSubMenuItem(newDns.Name, newDns.Name)
+
+			dnsMenus[newDns.Name] = dnsConfigSubMenu
+			localDns := newDns
+			dnsConfigMenu.SetTitle(fmt.Sprintf("DNS config: %v", newDns.Name))
+
+			go func(localDns dns.Dns) {
+				for {
+					<-dnsConfigSubMenu.ClickedCh
+					if systrayUI.connectedDnsConfiguration.Name != localDns.Name {
+						dnsService := dns.DnsService{}
+						_, err := dnsService.ChangeDns(dns.ResetDns, systrayUI.connectedDnsConfiguration)
+						if err != nil {
+							fmt.Println(err)
+							return
+						}
+					}
+
+					systrayUI.setIcon(false)
+					dnsConfigMenu.SetTitle(fmt.Sprintf("DNS config: %v", localDns.Name))
+					systrayUI.selectedDnsConfiguration = localDns
+				}
+
+			}(localDns)
+		}
+
+	}()
 
 	go func() {
 		for {
@@ -168,67 +232,6 @@ func (systrayUI *SystrayUI) addDnsConfigurations() error {
 
 	go func() {
 		for {
-			<-menuAdd.ClickedCh
-			fmt.Println("Add new dns")
-
-			// newDns := dns.Dns{Name: "MyTest", PrimaryDns: "185.51.200.2", SecendaryDns: "178.22.122.100"}
-			newDns := openCMDAndGetDNSData()
-
-			if dnsMenus[newDns.Name] != nil {
-				fmt.Println(newDns.Name + " existed")
-				return
-			}
-
-			err = systrayUI.dnsRepository.ModifyDnsConfigurations(newDns)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			systrayUI.selectedDnsConfiguration = newDns
-
-			dnsService := dns.DnsService{}
-			_, err := dnsService.ChangeDns(dns.SetDns, systrayUI.connectedDnsConfiguration)
-			if err != nil {
-				fmt.Println(err)
-
-				return
-			}
-
-			fmt.Println(newDns.Name + " connected successfully.")
-
-			systrayUI.setIcon(true)
-			systrayUI.setToolTip("connected to : " + newDns.Name)
-
-			dnsConfigSubMenu := dnsConfigMenu.AddSubMenuItem(newDns.Name, newDns.Name)
-
-			dnsMenus[newDns.Name] = dnsConfigSubMenu
-			localDns := newDns
-			dnsConfigMenu.SetTitle(fmt.Sprintf("DNS config: %v", newDns.Name))
-
-			go func(localDns dns.Dns) {
-				for {
-					<-dnsConfigSubMenu.ClickedCh
-					if systrayUI.connectedDnsConfiguration.Name != localDns.Name {
-						dnsService := dns.DnsService{}
-						_, err := dnsService.ChangeDns(dns.ResetDns, systrayUI.connectedDnsConfiguration)
-						if err != nil {
-							fmt.Println(err)
-							return
-						}
-					}
-
-					systrayUI.setIcon(false)
-					dnsConfigMenu.SetTitle(fmt.Sprintf("DNS config: %v", localDns.Name))
-					systrayUI.selectedDnsConfiguration = localDns
-				}
-
-			}(localDns)
-		}
-
-	}()
-
-	go func() {
-		for {
 			<-menuDelete.ClickedCh
 			fmt.Println("Delete Current Dns")
 
@@ -258,6 +261,7 @@ func (systrayUI *SystrayUI) addDnsConfigurations() error {
 					dnsConfigMenu.SetTitle("DNS config: -")
 				}
 			}
+			delete(dnsMenus, targetDns.Name)
 			dnsService := dns.DnsService{}
 			_, err = dnsService.ChangeDns(dns.ResetDns, systrayUI.connectedDnsConfiguration)
 			if err != nil {
@@ -274,50 +278,32 @@ func (systrayUI *SystrayUI) addDnsConfigurations() error {
 
 	}()
 
+	go func() {
+		for {
+			<-networkInterfaceMenu.ClickedCh
+			fmt.Println("Network Interface")
+
+		}
+
+	}()
+
+	go func() {
+		for {
+			<-toolsMenu.ClickedCh
+			fmt.Println("Tools")
+
+		}
+
+	}()
+
+	go func() {
+		for {
+			<-consoleMenu.ClickedCh
+			fmt.Println("console")
+
+		}
+
+	}()
+
 	return nil
-}
-
-func openCMDAndGetDNSData() dns.Dns {
-	cmdPath := filepath.Join(os.Getenv("SystemRoot"), "System32", "cmd.exe")
-	pr, pw := io.Pipe()
-	cmd := exec.Command(cmdPath)
-	cmd.Stdout = os.Stdout
-	cmd.Stdin = pr
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Start()
-	if err != nil {
-		fmt.Println("Error:", err)
-		return dns.Dns{}
-	}
-
-	defer pw.Close()
-
-	fmt.Print("Enter DNS Name:")
-	scanner := bufio.NewScanner(os.Stdin)
-	scanner.Scan()
-	dnsName := scanner.Text()
-
-	pw.Write([]byte(dnsName + "\n"))
-
-	fmt.Print("Enter Primary IP:")
-	scanner.Scan()
-	primaryDns := scanner.Text()
-
-	// Write the second data to the pipe
-	pw.Write([]byte(primaryDns + "\n"))
-
-	// Prompt the user for the third data
-	fmt.Print("Enter Secendary Dns:")
-	scanner.Scan()
-	secendaryDns := scanner.Text()
-
-	// Write the third data to the pipe
-	pw.Write([]byte(secendaryDns + "\n"))
-
-	// Close the write end of the pipe
-	pw.Close()
-
-	newDns := dns.Dns{Name: dnsName, PrimaryDns: primaryDns, SecendaryDns: secendaryDns}
-	return newDns
 }
